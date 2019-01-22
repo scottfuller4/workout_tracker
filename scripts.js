2f5b68063d98548e8bb3efa84c9cae0dcbc4c1b9
Scott <scottfuller4@gmail.com>
2019-01-22 16:55:29 -0500

    built out base html structure, added some initial styles

--- scripts.js
+++ scripts.js
@@ -1,18 +1,28 @@
 const App = {};
+App.apiKey = `4d8caf10dccace6d053c0c4de1a0dcb50297adf1`;
 
 $(function () {
     App.init();
 });
 
 App.init = function () {
-    App.getData();
+    // App.getMuscleGroup();
+}
+
+App.getMuscleGroup = function () {
+    $('#muscleGroup').on('submit', function (e) {
+        e.preventDefault();
+        const muscle = $("#muscleGroup option:selected").val();
+        console.log(muscle);
+    });
 }
 
 App.getData = function () {
     $.ajax({
-        url: `https://wger.de/api/v2/exercise`,
+        url: `https://wger.de/api/v2/exercise/?status=2`,
         method: 'GET'
     }).then(res => {
-        console.log('hello', res);
+        console.log('hello');
+        console.log(res);
     })
 }
